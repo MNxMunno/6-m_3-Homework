ad2c934e22e4fb91aa88e1aeacf49289bf996877
MNxMunno <bilolhuzayfa@gmmail.com>
2023-12-28 01:31:52 +0500

    change

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import logo from "./images/logo.svg";
+import bgImg from "./images/hero-bg-img.png";
+
+export { logo, bgImg };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,37 @@
 import "./Header.scss";
+import { logo } from "../../assets";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container">
+        <nav>
+          <a href="#" className="logo">
+            <img src={logo} alt="img" />
+          </a>
+          <div className="links">
+            <a href="#">Продукция</a>
+            <div className="brother">
+              <a href="#">Ламинатные тубы</a>
+              <a href="#">Экструзионные тубы</a>
+              <a href="#">Другая упаковка</a>
+            </div>
+            <a href="#">Сертификаты</a>
+            <a href="#">Наша команда</a>
+            <a href="#">О нас</a>
+            <a href="#">Новости</a>
+            <a href="#">Вакансии</a>
+            <a href="#">Контакты</a>
+          </div>
+          <div className="language">
+            <p>
+              <b>RU</b>/EN
+            </p>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
